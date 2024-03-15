d54578fc0a5081c438e6d8d86a28acc4d75ae57e
Michal Glinski <michig@onet.pl>
2024-03-15 01:35:55 +0100

    https://github.com/users/MichalLuczyszyn/projects/2?pane=issue&itemId=56513105

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -21,36 +21,58 @@ namespace Calculator
                     Console.WriteLine("The result of the multiplication is: " + (a * b));
                     break;
                 case 4:
-                    if (b == 0)
-                    {
-                        Console.WriteLine("Error! You cannot divide by 0.");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The result of the division is: " + (a / b));
-                        break;
-                    }
+                    Console.WriteLine("The result of the division is: " + (a / b));
+                    break;
             }
         }
 
-        public static void CheckOperations (int operation, double a, double b)
+
+        public static double CheckDoubleInput ()
         {
-            int i = 1;
-            while (i <= 4)
+            string input = Console.ReadLine();
+            double result;
+            while (!double.TryParse(input, out result))
             {
-                if (operation == i)
-                {
-                    MathCalculations.Calculate(operation, a, b);
-                    break;
-                }
-                i++;
+                Console.WriteLine("Error: Invalid character. Try again.");
+                input = Console.ReadLine();
             }
-            if (i == 5)
+            result = double.Parse(input);
+            return result;
+        }
+
+
+        public static int CheckOperationInput()
+        {
+            string input = Console.ReadLine();
+            int result;
+            while (true)
             {
-                Console.WriteLine("Error: Wrong operation: " + operation);
+                if (!int.TryParse(input, out result))
+                {
+                    Console.WriteLine("Error: Invalid character. Try again.");
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    result = int.Parse(input);
+                    int i = 1;
+                    while (i <= 4)
+                    {
+                        if (result == i)
+                        {
+                            return result;
+                        }
+                        i++;
+                        if (i == 5)
+                        {
+                            Console.WriteLine("Error: Wrong operation: " + result + ". Try again.");
+                            input = Console.ReadLine();
+                        }
+                    }
+                }
             }
         }
+
 
     }
     internal class Program
@@ -60,31 +82,43 @@ namespace Calculator
         {
             while (true)
             {
-                Console.WriteLine("Welcome to Calculator");
-                Console.WriteLine("Let's give the first argument!");
-                double a = 0;
-                double b = 0;
+                double a;
+                double b;
                 int operation = 0;
-                bool ifError = false;
-                try
+                Console.WriteLine("Welcome to Calculator");
+
+                Console.WriteLine("Let's give the first argument!");
+                a = MathCalculations.CheckDoubleInput();
+
+                Console.WriteLine("Choose the mathematical operation:");
+                Console.WriteLine("Write 1 for add \n Write 2 for subtract \n Write 3 for multiply \n Write 4 for divide");
+                operation = MathCalculations.CheckOperationInput();
+                switch (operation)
                 {
-                    a = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Choose the mathematical operation:");
-                    Console.WriteLine("Write 1 for add \n Write 2 for subtract \n Write 3 for multiply \n Write 4 for divide");
-                    operation = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter the second argument");
-                    b = double.Parse(Console.ReadLine());
+                    case 1:
+                        Console.WriteLine("Chosen operation: add");
+                        break;
+                    case 2:
+                        Console.WriteLine("Chosen operation: substract");
+                        break;
+                    case 3:
+                        Console.WriteLine("Chosen operation: multiply");
+                        break;
+                    case 4:
+                        Console.WriteLine("Chosen operation: divide");
+                        break;
+                }
+
+                Console.WriteLine("Enter the second argument");
+                b = MathCalculations.CheckDoubleInput();
                 
-                }
-                catch (FormatException)
+                while ((operation == 4) && (b == 0))
                 {
-                    Console.WriteLine("Error: Invalid character!");
-                    ifError = true;
+                    Console.WriteLine("Error: You cannot divide by 0. Give a different number.");
+                    b = MathCalculations.CheckDoubleInput();
                 }
-                if (ifError == false)
-                {
-                    MathCalculations.CheckOperations(operation, a, b);
-                }
+                MathCalculations.Calculate(operation, a, b);
+
                 Console.WriteLine("Press Enter to start a new calculation.");
                 Console.ReadLine();
                 Console.Clear();
